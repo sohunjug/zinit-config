75aecee846ae0460f4827d62307c6757246f2e7a
sohunjug <sohunjug@hotmail.com>
2021-10-11 10:33:14 +0800

    zinit

--- profile.zsh
+++ profile.zsh
@@ -1,4 +1,4 @@
-source $S_ZSH/proxy.zsh
+[[ ! -f $S_ZSH/proxy.zsh]] || source $S_ZSH/proxy.zsh
 ### Added by Zinit's installer
 if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
     print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
